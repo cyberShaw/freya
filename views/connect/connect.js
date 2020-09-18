98363d35ead350d808f702336e6c56ea33fdddfb
Saiharsha B <saibalsu@gmail.com>
2020-09-18 20:29:03 +0530

    Update alumni connect page ⚡

--- views/connect/connect.js
+++ views/connect/connect.js
@@ -1,37 +1,46 @@
-let connect = document.querySelector('.connect')
-let connectData = []
+let connect = document.querySelector('.connect');
+let connectData = [];
 
-let getData = async() => {
-    await fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-            return response.json();
-        })
-        .then(connectListings => {
-            console.log(connectListings);
-            connectData = connectListings;
-        })
-        .catch(error => {
-            console.log(error);
-        });
-        connectData.map(el => {
-            connect.innerHTML +=
-            `
+let getData = async () => {
+  await fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+      return response.json();
+    })
+    .then(connectListings => {
+      console.log(connectListings);
+      connectData = connectListings;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  connectData.map(el => {
+    connect.innerHTML +=
+      `
             <div class="card">
                 <div class="card-content">
+                    <article class="post">
                     <div class="media">
                         <div class="media-left">
-                        <p class="title is-4">`+el.title+`</p>
-                        <p class="subtitle is-6">Written by user: `+el.userId+`</p>
+                        <p class="title is-4">` +
+      el.title +
+      `</p>
+                        <p class="subtitle is-6">Written by user: ` +
+      el.userId +
+      `											<span class="tag is-danger">Comment</span>
+      </p>
                         </div>
                     </div>
                     <div class="content">
-                        `+el.body+`
+                        ` +
+      el.body +
+      `
                     </div>
+                    </article>
                 </div>
             </div>
             <br>
             `;
-        });
-    };
+  });
+};
 
 getData();
