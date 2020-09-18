ecf34b896557d0ba390a298c5a287fb974ded897
Bharath <bharathkrishnan99@gmail.com>
2020-09-18 18:59:46 +0530

    Search and Connect

---
+++ views/connect/connect.js
@@ -0,0 +1,37 @@
+let connect = document.querySelector('.connect')
+let connectData = []
+
+let getData = async() => {
+    await fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            return response.json();
+        })
+        .then(connectListings => {
+            console.log(connectListings);
+            connectData = connectListings;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+        connectData.map(el => {
+            connect.innerHTML +=
+            `
+            <div class="card">
+              <div class="card-content">
+                  <div class="media">
+                      <div class="media-left">
+                        <p class="title is-4">`+el.title+`</p>
+                        <p class="subtitle is-6">Written by user: `+el.userId+`</p>
+                        <div class="content">
+                            `+el.body+`
+                        </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
+            <br>
+            `;
+        });
+    };
+
+getData();
---
+++ views/search/search.js
@@ -0,0 +1,44 @@
+let search = document.querySelector('.search')
+let searchData = [];
+
+let getData = async() => {
+    await fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            return response.json();
+        })
+        .then(searchListings => {
+            console.log(searchListings);
+            searchData = searchListings;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+        searchData.map(el => {
+            search.innerHTML +=
+              `
+              <div class="card">
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-left">
+                            <p class="title is-4">`+el.name+`</p>
+                            <p class="subtitle is-6">@`+el.username+`</p>
+                            <p>Email: <a href = "mailto: `+el.email+`">`+el.email+`</a></p>
+                            <p>Phone: `+el.phone+`</p>
+                            <p>Address: `
+                            +el.address.street+`, `
+                            +el.address.suite+`, `
+                            +el.address.city+`, `
+                            +el.address.zipcode+`
+                            </p>
+                            <p>Website: <a href = "http://`+el.website+`">`+el.website+`</a></p>
+                            <p>Company: `+el.company.name+`</p>
+                        </div>
+                    </div>
+                </div>
+              </div>
+              <br>
+              `;
+        });
+};
+
+getData();
